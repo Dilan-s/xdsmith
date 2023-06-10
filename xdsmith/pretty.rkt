56288068e975af23ee6ecc490bdc5a87c725ed62
Sheth, Dilan <dilan.sheth19@imperial.ac.uk>
2023-06-10 13:24:14 +0100

    Update function method to function

--- xdsmith/pretty.rkt
+++ xdsmith/pretty.rkt
@@ -51,47 +51,47 @@
     [`(ProgramWithMain ,definitions ,main-body)
      (v-append
       (multi-line #<<EOF
-function method safeDivide (a : int, b : int) : int {
+function safeDivide (a : int, b : int) : int {
   if b == 0 then 0 else a / b
 }
 
-function method safeSeqRef<T> (s : seq<T>, i : int, default : T) : T {
+function safeSeqRef<T> (s : seq<T>, i : int, default : T) : T {
   if 0 <= i < |s| then s[i] else default
 }
 
-function method safeSeqSet<T> (s : seq<T>, i : int, val : T) : seq<T> {
+function safeSeqSet<T> (s : seq<T>, i : int, val : T) : seq<T> {
   if 0 <= i < |s| then s[i := val] else s
 }
 
-function method safeSeqTake<T> (s : seq<T>, x : int) : seq<T> {
+function safeSeqTake<T> (s : seq<T>, x : int) : seq<T> {
   if 0 <= x <= |s| then s[..x] else s
 }
 
-function method safeSeqDrop<T> (s : seq<T>, x : int) : seq<T> {
+function safeSeqDrop<T> (s : seq<T>, x : int) : seq<T> {
   if 0 <= x <= |s| then s[x..] else s
 }
 
-function method safeSeqSubseq<T> (s : seq<T>, x : int, y : int) : seq<T> {
+function safeSeqSubseq<T> (s : seq<T>, x : int, y : int) : seq<T> {
   if 0 <= x <= y <= |s| then s[x..y] else s
 }
 
-function method safeSeqSlice1Colon<T> (s : seq<T>, x : int) : seq<seq<T>> {
+function safeSeqSlice1Colon<T> (s : seq<T>, x : int) : seq<seq<T>> {
   if 0 <= x <= |s| then s[x:] else [s]
 }
 
-function method safeSeqSlice2<T> (s : seq<T>, x : int, y: int) : seq<seq<T>> {
+function safeSeqSlice2<T> (s : seq<T>, x : int, y: int) : seq<seq<T>> {
   if 0 <= x && 0 <= y && x + y <= |s| then s[x:y] else [s]
 }
 
-function method safeSeqSlice3<T> (s : seq<T>, x : int, y : int, z : int) : seq<seq<T>> {
+function safeSeqSlice3<T> (s : seq<T>, x : int, y : int, z : int) : seq<seq<T>> {
   if 0 <= x && 0 <= y && 0 <= z && x + y + z <= |s| then s[x:y:z] else [s]
 }
 
-function method safeSeqSlice3Colon<T> (s : seq<T>, x : int, y : int, z : int) : seq<seq<T>> {
+function safeSeqSlice3Colon<T> (s : seq<T>, x : int, y : int, z : int) : seq<seq<T>> {
   if 0 <= x && 0 <= y && 0 <= z && x + y + z <= |s| then s[x:y:z:] else [s]
 }
 
-function method lengthNormalize (x : int) : nat {
+function lengthNormalize (x : int) : nat {
   (if x < 0 then -x else x) % 50
 }
 
@@ -109,7 +109,7 @@ EOF
                (pretty meth))]
 
     [`(TopLevelFunMethodDefinition ,name ,meth)
-     (h-append (text "function method ")
+     (h-append (text "function ")
                (text name)
                space
                (pretty meth))]
